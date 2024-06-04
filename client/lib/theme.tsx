54690e5ed59fb375b373394f3e66c7b71017b21e
lxchurbakov <lxchurbakov@gmail.com>
2024-06-04 08:00:25 +0300

    Add atoms
    Here I add basic components like <Text>, <Flex> and <Container> that I made myself for one of my previous projects using styled-components. It is not necessary, of course, but I just want to use as little of 3rd party libraries as possible

---
+++ client/lib/theme.tsx
@@ -0,0 +1,17 @@
+export const font = {
+    family: 'Rubik, sans',
+};
+
+export const colors = {
+    black: '#111511',
+    text: '#222222',
+    white: '#ffffff',
+    card: '#f0f0f0',
+    carder: '#f6f6f6',
+
+    blue: '#4147d3',
+    bluer: '#11157B',
+    bluest: '#4248D5',
+
+    red: '#ff1050',
+};
